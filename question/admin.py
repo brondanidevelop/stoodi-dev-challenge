50e3f7213a3dc20e69ccb06cda2adc968ef0bf2e
Marcelo Moro Brondani <mbrondani@inf.ufsm.br>
2022-07-25 16:42:59 -0300

    feat: História 1 - Configurado interface via django admin para cadastro das questões

--- question/admin.py
+++ question/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Question
 # Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text','order', 'is_active', 'create_at','update_at','user_create','user_update')
+
+    def save_model(self, request, obj, form, change):
+        print(obj)
+        if change:
+            obj.user_update = request.user
+        else:
+            obj.user_create = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Question, QuestionAdmin)
