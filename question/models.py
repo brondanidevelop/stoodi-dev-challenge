6f0607122901366449b1d7e2a3d8664e92e85e97
Marcelo Moro Brondani <mbrondani@inf.ufsm.br>
2022-07-25 17:45:27 -0300

    feat: História 1 - Criado modelo para as respostas no banco de dados

--- question/models.py
+++ question/models.py
@@ -20,3 +20,37 @@ class Question(Log_Question):
     
     def __str__(self):
         return self.question_text
+
+
+LETTERS_CHOICES = [
+    ('0', 'a'),
+    ('1', 'b'),
+    ('2', 'c'),
+    ('3', 'd'),
+    ('4', 'e'),
+    ('5', 'f'),
+    ('6', 'g'),
+    ('7', 'h'),
+    ('8', 'i'),
+    ('9', 'j'),
+]
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+    order = models.CharField(max_length=1, choices=LETTERS_CHOICES, default='a')
+    is_correct_answer = models.BooleanField(default=False)
+    create_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    update_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    is_active = models.BooleanField(default=True)
+    user_create =  models.ForeignKey(User, related_name='Answer_user_create', on_delete=models.CASCADE, editable=False, null=True, blank=True)
+    user_update =  models.ForeignKey(User, related_name='Answer_user_update', on_delete=models.CASCADE, editable=False, null=True, blank=True)
+    
+    def __str__(self):
+        return self.answer_text
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['question', 'is_correct_answer'], name='unique_correct_answer'),
+            models.UniqueConstraint(fields=['question', 'answer_text'], name='unique_answer_iqual')
+        ]
