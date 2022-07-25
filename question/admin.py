cd150fd0afa7393b524da413214ecc84821f2b74
Marcelo Moro Brondani <mbrondani@inf.ufsm.br>
2022-07-25 17:46:02 -0300

    feat: História 1 - Configurado interface via django admin para cadastro das respostas

--- question/admin.py
+++ question/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Answer
 # Register your models here.
 
 
@@ -15,4 +15,15 @@ class QuestionAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question','answer_text','order','is_correct_answer','create_at','update_at','is_active','user_create','user_update')
+    def save_model(self, request, obj, form, change):
+        print(obj)
+        if change:
+            obj.user_update = request.user
+        else:
+            obj.user_create = request.user
+        super().save_model(request, obj, form, change)
+
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
