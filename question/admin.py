e282266cd962dc684e10721e4bcdfa8105f05715
Marcelo Moro Brondani <mbrondani@inf.ufsm.br>
2022-07-25 21:49:44 -0300

    refactor: História 2 - removido print desnecessário

--- question/admin.py
+++ question/admin.py
@@ -7,7 +7,6 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('id', 'question_text','order', 'is_active', 'create_at','update_at','user_create','user_update')
 
     def save_model(self, request, obj, form, change):
-        print(obj)
         if change:
             obj.user_update = request.user
         else:
@@ -17,8 +16,8 @@ class QuestionAdmin(admin.ModelAdmin):
 
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ('id', 'question','answer_text','order','is_correct_answer','create_at','update_at','is_active','user_create','user_update')
+    
     def save_model(self, request, obj, form, change):
-        print(obj)
         if change:
             obj.user_update = request.user
         else:
