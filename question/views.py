510656b430f64712acd5f28b209f58da962e19fb
Marcelo Moro Brondani <mbrondani@inf.ufsm.br>
2022-07-26 23:25:14 -0300

    feat: História 4 - Desenvolvido recurso na correção para possibilitar avançar e voltar na questão

--- question/views.py
+++ question/views.py
@@ -45,3 +45,38 @@ def question_answer(request):
     }
 
     return render(request, 'question/answer.html', context=context)
+
+def check_question(question_order, answer_selected):
+    answer = Answer.objects.filter(
+        question__order=question_order,
+        order__exact=answer_selected,
+        is_active=True,
+        is_correct_answer=True).exists()
+
+    return answer
+
+def next_question(question_order):
+    queryset = Question.objects.filter(
+        is_active=True,
+        order__gt=question_order
+    )
+    if queryset.exists():
+        queryset = queryset.only('id', 'order').order_by('order')
+        return queryset[0].order
+
+    return 1
+
+def question_answer(request):
+    question_order = request.POST.get('question_order', None)
+    question_ref = request.POST.get('question_ref', None)
+    answer_selected = request.POST.get('answer', 'z')
+
+    is_correct = check_question(question_order, answer_selected)
+    question = next_question(int(question_order)) if is_correct else question_order
+
+    context = {
+        'is_correct': is_correct,
+        'next': question
+    }
+
+    return render(request, 'question/answer.html', context=context)
